7393c92eadee81cb33aadfdc1ddcb6b1ff099953
Blake Heller <hellerblake@gmail.com>
2022-12-31 16:15:26 -0800

    add sample game logic

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as dog from "./sprite-animations/dogAnimation.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas =document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
 
-setupCounter(document.querySelector('#counter'))
+const playerImage = new Image();
+playerImage.src = '/shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+let gameFrame = 0;
+let staggerFrames = 5;
+let spriteAnimations = [];
+let playerState = 'roll';
+
+dog.states.forEach((state,index) => {
+    let frames = {
+        loc: [],
+    }
+    for(let i = 0; i < state.frames; i++){
+        let positionX = i * spriteWidth;
+        let positionY = index * spriteHeight
+        frames.loc.push({x: positionX, y: positionY})
+    }
+    spriteAnimations[state.name] = frames;
+})
+    console.log(spriteAnimations)
+function animate(){
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position= Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteAnimations[playerState].loc[position].x;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
---
+++ sprite-animations/dogAnimation.js
@@ -0,0 +1,42 @@
+export const states = [
+    {
+        name: 'idle',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 11
+    },
+    {
+        name: 'getHit',
+        frames: 4
+    },
+];
